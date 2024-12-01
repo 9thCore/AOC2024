97dce718a9dfccc57b84520984dba13ee5accebd
9thCore <38700349+9thCore@users.noreply.github.com>
2024-12-01 17:23:45 +0200

    day1 part2

--- Day1/Day1.cpp
+++ Day1/Day1.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <map>
 #include <algorithm>
 std::ifstream fin("input.txt");
 
 void part1(std::vector<int>, std::vector<int>);
+void part2(std::vector<int>, std::vector<int>);
 
 int main()
 {
@@ -20,7 +22,7 @@ int main()
 	part1(firstList, secondList);
 	std::cout << "\n";
 
-	// part2(firstList, secondList);
+	part2(firstList, secondList);
 }
 
 void part1(std::vector<int> pFirstList, std::vector<int> pSecondList) {
@@ -34,4 +36,19 @@ void part1(std::vector<int> pFirstList, std::vector<int> pSecondList) {
 	}
 
 	std::cout << "part 1: " << sum;
+}
+
+void part2(std::vector<int> pFirstList, std::vector<int> pSecondList) {
+	std::map<int, int> count;
+	unsigned long long sum = 0;
+
+	for (const int x : pSecondList) {
+		count[x]++;
+	}
+
+	for (const int x : pFirstList) {
+		sum += (unsigned long long)x * count[x];
+	}
+
+	std::cout << "part 2: " << sum;
 }
