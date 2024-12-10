ce5d0efd5c301f1f4326fa285558dd9debe0085f
9thCore <38700349+9thCore@users.noreply.github.com>
2024-12-10 13:49:25 +0200

    day10 part2
    slight bit of rewrite, oh well

--- Day10/Day10.cpp
+++ Day10/Day10.cpp
@@ -19,6 +19,7 @@ typedef std::vector<cell> line;
 typedef std::vector<line> data;
 
 void part1(data);
+void part2(data);
 
 int main()
 {
@@ -35,9 +36,10 @@ int main()
 
 	part1(input);
 	std::cout << "\n";
+	part2(input);
 }
 
-int search_from(data &pInput, int pI, int pJ, int pLastHeight, int pTargetHeight) {
+int search_from(data &pInput, bool pMarkPeak, int pI, int pJ, int pLastHeight, int pTargetHeight) {
 	if (pI < 0 || pI >= pInput.size()
 		|| pJ < 0 || pJ >= pInput[pI].size()
 		|| pInput[pI][pJ].visited
@@ -46,7 +48,9 @@ int search_from(data &pInput, int pI, int pJ, int pLastHeight, int pTargetHeight
 	}
 
 	if (pInput[pI][pJ].height == pTargetHeight) {
-		pInput[pI][pJ].visited = true;
+		if (pMarkPeak) {
+			pInput[pI][pJ].visited = true;
+		}
 		return 1;
 	}
 
@@ -55,7 +59,7 @@ int search_from(data &pInput, int pI, int pJ, int pLastHeight, int pTargetHeight
 	int result = 0;
 
 	for (int i = 0; i < ORTHOGONAL_DIR_COUNT; i++) {
-		result += search_from(pInput, pI + DELTA_ORTHOGONAL[i][0], pJ + DELTA_ORTHOGONAL[i][1], pLastHeight + 1, pTargetHeight);
+		result += search_from(pInput, pMarkPeak, pI + DELTA_ORTHOGONAL[i][0], pJ + DELTA_ORTHOGONAL[i][1], pLastHeight + 1, pTargetHeight);
 	}
 
 	pInput[pI][pJ].visited = false;
@@ -63,8 +67,8 @@ int search_from(data &pInput, int pI, int pJ, int pLastHeight, int pTargetHeight
 	return result;
 }
 
-int search_from(data &pInput, int pI, int pJ, int pTargetHeight) {
-	return search_from(pInput, pI, pJ, pInput[pI][pJ].height - 1, pTargetHeight);
+int search_from(data &pInput, bool pMarkPeak, int pI, int pJ, int pTargetHeight) {
+	return search_from(pInput, pMarkPeak, pI, pJ, pInput[pI][pJ].height - 1, pTargetHeight);
 }
 
 void part1(data pInput) {
@@ -73,7 +77,7 @@ void part1(data pInput) {
 	for (int i = 0; i < pInput.size(); i++) {
 		for (int j = 0; j < pInput[i].size(); j++) {
 			if (pInput[i][j].height == 0) {
-				sum += search_from(pInput, i, j, 9);
+				sum += search_from(pInput, true, i, j, 9);
 
 				for (int i = 0; i < pInput.size(); i++) {
 					for (int j = 0; j < pInput[i].size(); j++) {
@@ -85,4 +89,18 @@ void part1(data pInput) {
 	}
 
 	std::cout << "part 1: " << sum;
+}
+
+void part2(data pInput) {
+	int sum = 0;
+
+	for (int i = 0; i < pInput.size(); i++) {
+		for (int j = 0; j < pInput[i].size(); j++) {
+			if (pInput[i][j].height == 0) {
+				sum += search_from(pInput, false, i, j, 9);
+			}
+		}
+	}
+
+	std::cout << "part 2: " << sum;
 }
