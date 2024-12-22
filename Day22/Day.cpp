9cf4651bcb559a426c1eadf68f1582c10485c67e
9thCore <38700349+9thCore@users.noreply.github.com>
2024-12-22 12:53:22 +0200

    day22 p2 runtime improved to about 14s
    - packing the hashes better (5 bits per value, instead of 8)
    - instead of a map of hash -> sum, use an array such that access and "insertion" is slightly faster
    - instead of a set of hashes, use an array of unsigned, 32-wide integers and use bitwise operations to enable and disable "flags" on them; each integer holds 32 such flags

--- Day22/Day.cpp
+++ Day22/Day.cpp
@@ -4,14 +4,13 @@
 #include <regex>
 #include <vector>
 #include <deque>
-#include <unordered_map>
-#include <unordered_set>
+#include <array>
 std::ifstream fin("input.txt");
 
 typedef long long num;
 typedef std::vector<num> data;
 typedef std::deque<num> window;
-typedef uint64_t window_hash;
+typedef uint32_t window_hash;
 
 constexpr num MAGIC_NUMBER = 16777216 - 1;
 
@@ -73,38 +72,59 @@ num calculate_change(num pLast, num pCurrent) {
 }
 
 window_hash get_value(window &pDeque) {
-    // Assume the change will always fit in 8 bits,
-    //  due to it being in the range [-9, 9].
-    uint8_t val = pDeque.back();
+    // Assume the change will always be
+    //  in the range [-9, 9], so
+    //  pack it into 5 bits:
+    //  0: 00000, +1: 00001, ...
+    // +9: 01001, -1: 01010,
+    // -2: 01011, ...
+    // -6: 01111, -7: 10000,
+    // -8: 10001, -9: 10010
+    num val = pDeque.back();
     pDeque.pop_back();
+    if (val < 0) {
+        return 9 - val;
+    }
     return val;
 }
 
+// 20 bit number that represents
+//  a given sequence, uniquely.
 window_hash hash_window(window pWindow) {
     return get_value(pWindow)
-        | (get_value(pWindow) << 8)
-        | (get_value(pWindow) << 16)
-        | (get_value(pWindow) << 24);
+        | (get_value(pWindow) << 5)
+        | (get_value(pWindow) << 10)
+        | (get_value(pWindow) << 15);
 }
+
+// Maps a sequence of changes to
+//  the final sum, from which we grab
+//  the highest value.
+std::array<uint16_t, 0xFFFFF + 1> sequence_map = { 0 };
+
+// "Set" of already added hashes for
+//  a given buyer, using bitwise
+//  operations to pack 32 flags
+//  into one 32-wide unsigned integer.
+std::array<uint32_t, 0x7FFF + 1> added_map = { 0 };
 
 void part2(data pInput) {
     constexpr int SUCCESSIVE_CHANGE_COUNT = 4;
-    
-    // Maps a sequence of changes to
-    //  the final sum, from which we grab
-    //  the highest value.
-    std::unordered_map<window_hash, num> buyer_map;
+
+    // Amortises the cost of finding
+    //  the best sequence a bit.
+    window_hash biggest_global_hash = 0;
 
     int idx = 0;
     for (num &secretNumber : pInput) {
-        // Map used to denote if we have added
-        //  a given hash to the global sum
-        std::unordered_set<window_hash> added;
-
         // A sliding window of the current sequence
         //  of changes, using a deque to leverage
         //  the fast popping from the front.
         window sliding_window;
+
+        // Amortises the cost of resetting
+        //  the added map a bit.
+        window_hash biggest_hash = 0;
 
         // Prepare the change sliding windows
         //  by simulating the first changes
@@ -117,22 +137,36 @@ void part2(data pInput) {
         // Slide window along and map values accordingly
         for (int step = SUCCESSIVE_CHANGE_COUNT; step < 2000; step++) {
             window_hash hash = hash_window(sliding_window);
-            if (added.find(hash) == added.end()) {
-                buyer_map[hash] += secretNumber % 10;
-                added.insert(hash);
+
+            window_hash key = hash / 32;
+            window_hash bit = 1 << (hash % 32);
+
+            // Check if the current hash has already been added,
+            //  and if not, we add it
+            if ((added_map[key] & bit) == 0) {
+                sequence_map[hash] += secretNumber % 10;
+                added_map[key] |= bit;
             }
 
+            biggest_hash = std::max(biggest_hash, hash);
             num last = secretNumber;
             evolve_secret(secretNumber);
 
             sliding_window.pop_front();
             sliding_window.push_back(calculate_change(last, secretNumber));
         }
+
+        window_hash key = biggest_hash / 32;
+        for (window_hash i = 0; i < key; i++) {
+            added_map[i] = 0;
+        }
+
+        biggest_global_hash = std::max(biggest_global_hash, biggest_hash);
     }
 
-    num result = 0;
-    for (const auto &iter : buyer_map) {
-        result = std::max(result, iter.second);
+    uint16_t result = 0;
+    for (window_hash i = 0; i < biggest_global_hash; i++) {
+        result = std::max(sequence_map[i], result);
     }
 
     std::cout << "part 2: " << result;
